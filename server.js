1fdebbbee0a3a8945f488e1ac3fb0c5e09404ad8
PupS1k <denikpro@mail.ru>
2019-08-13 16:24:56 +0300

    install nodemon and modules necessary for mongodb

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.use(bodyParser.json());
 // });
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin");
+  res.header("Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin");
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
