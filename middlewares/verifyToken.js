a5ee1cafc6ba74301dd1b905445c6a66c2a68098
PupS1k <denikpro@mail.ru>
2019-10-06 14:19:59 +0300

    refactoring handle error of verify token

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,22 +1,24 @@
-const jwt = require('jsonwebtoken')
-const config = require('../config')
+const jwt = require('jsonwebtoken');
+const config = require('../config');
 
 module.exports = function (req, res, next) {
-	const bearer = req.header('Authorization').split(' ')
+	const bearer = req.header('Authorization').split(' ');
 	const token = bearer[1];
 
-	if (token === null) return res.status(403).send('Access Denied')
+	if (token === null) return res.status(403).send('Access Denied');
 
 	try {
-		const dataToken = jwt.verify(token, config.secretKey)
-		req.user = {
-			id: dataToken.id,
-			mail: dataToken.mail,
-			password: dataToken.password,
-		  	sellerType: dataToken.sellerType,
-		}
-		next()
+		jwt.verify(token, config.secretKey, (err, decoded) => {
+		  if(err) res.status(400).send('Invalid token');
+		  req.user = {
+				id: decoded.id,
+				mail: decoded.mail,
+				password: decoded.password,
+				sellerType: decoded.sellerType,
+		  };
+		  next();
+		});
 	} catch (err) {
-		res.status(400).send('Invalid token')
+		next(err);
 	}
-}
+};
