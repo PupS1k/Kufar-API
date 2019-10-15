93527235627c30b833e38113031f7dad021b6949
PupS1k <denikpro@mail.ru>
2019-10-15 11:56:02 +0300

    delete headers for CORS

--- app.js
+++ app.js
@@ -19,15 +19,8 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Credentials', true);
-	res.header('Access-Control-Allow-Origin', req.headers.origin);
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization');
-	next();
-});
 
-app.use('/', router);
+app.use('/api', router);
 
 // error handler
 app.use(function (err, req, res, next) {
