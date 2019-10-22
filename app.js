d959552ac0dd216da63da326f155ae9669f632f2
PupS1k <denikpro@mail.ru>
2019-10-23 00:37:03 +0300

    revert last commit

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client')));
-app.use(express.static(path.join(__dirname, 'routes')));
 
 
 app.get('/', function(req, res) {
