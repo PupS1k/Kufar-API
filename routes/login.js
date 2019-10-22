c2970c868abf6dcc16654a532d43766f1f043ffa
PupS1k <denikpro@mail.ru>
2019-10-23 00:58:31 +0300

    change proxy

--- routes/login.js
+++ routes/login.js
@@ -6,7 +6,6 @@ const sendToken = async (req, res, next) => {
 		const user = await User.find({mail: req.body.mail, password: req.body.password})
 		  .then(users => users[0]);
 		if (!user) {
-
 		  return res.status(400).json({message: 'E-mail или пароль введены не верно'});
 		}
 
