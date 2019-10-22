1806f6c2ac97782073f51c8bb481d093c5a06c60
PupS1k <denikpro@mail.ru>
2019-10-23 00:17:55 +0300

    add epxress.static 'routes'

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'routes')));
 
 
 app.get('/', function(req, res) {
