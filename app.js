d167d2b70ed5be8e85db32807b81f500e86967bb
PupS1k <denikpro@mail.ru>
2019-10-23 13:43:35 +0300

    delete json()

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ setUpConnection();
 app.use(fileUpload({createParentPath: true}));
 
 app.use(logger('dev'));
-// app.use(express.json());
+app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client')));
