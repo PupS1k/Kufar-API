af495c3653dcb780c1389be78925d880eff9e764
PupS1k <denikpro@mail.ru>
2019-10-26 20:14:30 +0300

    handle error in route 'images'

--- routes/images.js
+++ routes/images.js
@@ -1,8 +1,12 @@
 const Image = require('../db/models/Image');
 
 const sendImage =  async(req, res, next) => {
-	const image = await Image.findOne({_id: req.params.fileName}).then(img => img.body);
-	res.send(JSON.stringify(image));
+	try {
+		const image = await Image.findOne({_id: req.params.fileName}).then(img => img.body);
+		res.send(JSON.stringify(image));
+	} catch (err) {
+		next(err);
+	}
 };
 module.exports = {sendImage};
 
